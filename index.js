7e7b85631987640577c64a0dda93960d52bac355
Vishal verma <132982737+vishal-kr007@users.noreply.github.com>
2023-10-27 00:38:33 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "react";
+//..................................................here is the tooltip class based component.......................................................
+
+class Tooltip extends React.Component {
+  render() {
+    const { position } = this.props;
+    return (
+      <div className={`${position}`}>
+        You are hovering "{position}" position!
+      </div>
+    );
+  }
+}
+
+export default Tooltip;
